09d483bf527d1c7b551225dd7dbdea39972347d2
neilSoftWorks <neilbay@gmail.com>
2025-03-05 09:00:19 +1300

    initial fixes

--- ApplicationSystem/src/main/java/com/example/applicationsystem/ApplicationSystemApplication.java
+++ ApplicationSystem/src/main/java/com/example/applicationsystem/ApplicationSystemApplication.java
@@ -2,12 +2,13 @@ package com.example.applicationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example"}) // Add this annotation
 public class ApplicationSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationSystemApplication.class, args);
     }
-
 }
